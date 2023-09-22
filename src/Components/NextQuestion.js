4c345238091f4b061b557fd03eb2409e21ffc1ab
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-22 18:56:30 +0100

    Feat: Next Question component

--- src/Components/NextQuestion.js
+++ src/Components/NextQuestion.js
@@ -13,7 +13,7 @@ function NextQuestion({ dispatch, answer, index,numQuestions }) {
   if(index === numQuestions - 1) return (
     <button
       className="btn btn-ui"
-      onClick={() => dispatch({ type: "finished" })}
+      onClick={() => dispatch({ type: "completed" })}
     >
       Finish
     </button>
