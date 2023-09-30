81622037ed623ea19aa32e15dfe105564dcbea96
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-30 21:57:02 +0100

    State management(no context API)

---
+++ src/Context/QuizContext.js
@@ -0,0 +1,129 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+
+const QuizContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  // Status: 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+
+    case "completed":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "newAnswer":
+      const curQuestion = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === curQuestion.correctOption
+            ? state.points + curQuestion.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    // return {
+    //   ...state,
+    //   status: "ready",
+    //   index: 0,
+    //   answer: null,
+    //   points: 0,
+    //   highscore: 0,
+    // };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
+function QuizProvider({ children }) {
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`${BASE_URL}/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  // Derived State
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
+
+  return (
+    <div>
+      <QuizContext.Provider
+        value={{
+          status,
+          questions,
+          index,
+          answer,
+          points,
+          highscore,
+          secondsRemaining,
+          numQuestions,
+          maxPossiblePoints,
+          dispatch
+        }}
+      >
+        {children}
+      </QuizContext.Provider>
+    </div>
+  );
+}
+
+function useQuiz() {
+  const context = useContext(QuizContext);
+  if (context === undefined)
+    throw new Error("QuizContext was used outside of QuizProvider");
+  return context;
+}
+
+export { QuizProvider, useQuiz };
