2423b7551e28e5927fe7f1278734d9832a40906c
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-30 21:54:55 +0100

    Refactored into Advanced state management

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./Components/App";
 import "./index.css";
+import { QuizProvider } from "./Context/QuizContext";
 
 // REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </React.StrictMode>
 );
 
